bc20b260237622097d0bc83a00ff9ff28afa434b
John0615 <zsp0509@LAPTOP-9TCG46D8.localdomain>
2026-01-04 16:50:19 +0800

    style: 样式调整

--- app/src/components/ui/modal.rs
+++ app/src/components/ui/modal.rs
@@ -1,14 +1,10 @@
 use leptos::prelude::*;
 
-
 #[component]
-pub fn Modal(
-    show: RwSignal<bool>,
-    children: Children
-) -> impl IntoView {
+pub fn Modal(show: RwSignal<bool>, children: Children) -> impl IntoView {
     view! {
         <dialog open=move || show.get() class="modal modal-bottom sm:modal-middle">
-            <div class="modal-box w-11/12 max-w-5xl">
+            <div class="modal-box w-11/12">
                 <button
                     class="btn btn-sm btn-circle absolute right-2 top-2"
                     on:click=move |_| show.set(false)
